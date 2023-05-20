a12f6942ac0358ab616bb0461900a1027ee55cd8
Dimas agung setyobudi <agungdimas390@gmail.com>
2023-05-20 14:09:36 +0700

    change username tb user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('employee_code');
+            $table->string('username')->comment('employee code');
             $table->string('password');
             $table->string('user_updated')->nullable();
             $table->rememberToken();
