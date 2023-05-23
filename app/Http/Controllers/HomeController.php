633fc53059f0a81c8ef1ef03c88aa5c4f114fda0
Dimas agung setyobudi <agungdimas390@gmail.com>
2023-05-24 06:25:00 +0700

    Update Layouts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // return User::with('employee')->get();
+        // return view('layouts.default');
         return view('home');
     }
 }
